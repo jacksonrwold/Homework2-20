6efe8d532b6b7f374e9b8231290e8c33ba9eccf1
jacksonrwold <jacksonrwold@gmail.com>
2019-02-21 09:06:08 -0700

    commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default class App extends Component {
+import Counter from "./counter";
+import Home from "./home";
+import page2 from "./page2";
+import page3 from "./page3";
+import page4 from "./page4";
+
+export default class NavigationComponent extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
+        <div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/page2" component={page2} />
+                        <Route path="/page3" component={page3} />
+                        <Route path="/page4" component={page4} />
+                    </Switch>
+                </div>
+            </Router>
+        </div>
     );
   }
 }
